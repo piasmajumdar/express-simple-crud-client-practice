4142bc8fe7bd34d9f206f487db84387565b77721
piasmajumdar <piasmajumdar@gmail.com>
2026-05-17 15:09:32 +0530

    Add user functionalities completed

--- src/app/lib/action.js
+++ src/app/lib/action.js
@@ -1,15 +1,34 @@
 import { revalidatePath } from "next/cache";
 
-export const deleteUser = async(userId)=>{
+export const deleteUser = async (userId) => {
     "use server"
     const req = await fetch(`http://localhost:5002/users/${userId}`, {
         method: 'DELETE',
     });
     const res = await req.json();
-    console.log(res);
+    // console.log(res);
 
     //TODO: revalidate path
-    if(res.deletedCount > 0){
+    if (res.deletedCount > 0) {
         revalidatePath('/users');
+    }
+}
+
+
+export const addUser = async (formData) => {
+    "use server"
+    const newUser = Object.fromEntries(formData.entries());
+    const req = await fetch('http://localhost:5002/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    });
+    const res = await req.json();
+    // console.log(res);
+
+    if(res.insertedId){
+        revalidatePath('/users')
     }
 }
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,13 +1,17 @@
 import AllUserTable from "@/components/AllUserTable";
 import { getUsers } from "../lib/data";
-import { deleteUser } from './../lib/action';
+import { addUser, deleteUser } from './../lib/action';
+import AddUserModal from "@/components/AddUserModal";
 
 const UsersPage = async() => {
     const users = await getUsers();
 
     return (
         <div className='w-11/12 mx-auto'>
-            <h2>All Users: {users.length}</h2>
+            <div className="flex justify-between py-10">
+                <h2>All Users: {users.length}</h2>
+                <AddUserModal addUserAction={addUser}></AddUserModal>
+            </div>
 
             <AllUserTable users={users} deleteUserAction={deleteUser}></AllUserTable>
         </div>
